e0e5c2e1c2e3538a60a0f1ef0583733f9f187972
WoodProgrammer <emirozbir@hotmail.com>
2018-04-12 02:11:59 +0300

    Server Tokens Disabled

--- servers_token/servers_token.sh
+++ servers_token/servers_token.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+echo "Which config file you want to add ? "
+read service_config_path
 
+echo "Config file_name "
+read service_config_file
+
+server_line_main=$(grep -i -n "listen " $service_config_file | awk '{print $1} '| sed 's/[^0-9]*//g' |awk '$0>x{x=$0};END{print x}')
+
+
+sed -i ' '"${server_line_main}"' i\server_tokens        off; \'  $service_config_path
